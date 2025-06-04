d13a2cbf4a63a70bf6ce3e830e4a798ba581da92
Olausz <2023211510080@iesp.edu.br>
2025-06-04 07:38:16 -0300

    Importando imagens

--- site-uniesp/src/App.jsx
+++ site-uniesp/src/App.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
-
+import Inicial from './pages/Inicial'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Container } from 'React-bootstrap'
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Container className='my-04'>
+        <Routes>
+          <Route path='/' element={< Inicial />} />
+        </Routes>
+      </Container>
+    </BrowserRouter>
   )
 }
 
---
+++ site-uniesp/src/pages/VisualizarNotas.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VisualizarNotas = () => {
+  return (
+    <div>VisualizarNotas</div>
+  )
+}
+
+export default VisualizarNotas
