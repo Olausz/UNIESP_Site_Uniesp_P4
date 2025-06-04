6327c23c0322f5595f104f98d625d7a557e4a6f8
Olausz <2023211510080@iesp.edu.br>
2025-06-04 09:07:01 -0300

    Terminando o codigo

--- site-uniesp/src/App.jsx
+++ site-uniesp/src/App.jsx
@@ -5,6 +5,8 @@ import { Container } from 'react-bootstrap'
 import CustomNavbar from './components/CustomNavbar'
 import Faculdade from './pages/Faculdade'
 import DpoLgpd from './pages/DpoLgpd'
+import Noticias from './pages/Noticias'
+import VisualizaNoticia from './pages/VisualizarNotas'
 
 
 const App = () => {
@@ -16,6 +18,8 @@ const App = () => {
           <Route path='/' element={< Inicial />} />
           <Route path="/a-faculdade" element={<Faculdade/>} />
           <Route path="/dpo-lgpd" element={<DpoLgpd/>}/>
+          <Route path="/noticias" element={<Noticias/>}/>
+          <Route path="/visualiza-noticia/:id" element={<VisualizaNoticia/>}/>
         </Routes>
       </Container>
     </BrowserRouter>
--- site-uniesp/src/pages/VisualizarNotas.jsx
+++ site-uniesp/src/pages/VisualizarNotas.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
+import axios from 'axios'
+import { Card, Container } from 'react-bootstrap'
 
-const VisualizarNotas = () => {
-  return (
-    <div>VisualizarNotas</div>
+const VisualizaNoticia = () => {
+
+    const { id } = useParams()
+    const [noticia, setNoticia] = useState({})
+    const urlBase = "http://localhost:3000"
+   
+   useEffect(() => {
+    axios.get(`${urlBase}/noticias/${id}`)
+    .then(response => setNoticia(response.data))
+    .catch(error => console.error("Erro ao ober notícia: ", error))
+   }, [id])
+   
+    return (
+        <Container className="py-4">
+            <Card className="shadow=sm">
+                <Card.Body>
+                    <Card.Title>{noticia.titulo}</Card.Title>
+                    <Card.Text>{noticia.texto}</Card.Text>
+                </Card.Body> 
+            </Card>
+
+        </Container>
   )
 }
 
-export default VisualizarNotas
+export default VisualizaNoticia
